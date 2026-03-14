6d993fe772d4e5b5e4c10af28a12f37a78edd41a
YADHARDHA LINGALA <125679462+Yadhardha@users.noreply.github.com>
2026-03-14 19:36:00 +0100

    Delete monitor directory

--- monitor/log_generator.py
+++
@@ -1,9 +0,0 @@
-import time
-import random
-log_file = "/tmp/devops.log"
-paths=["/","/login","/dashboard","/api/data"]
-while True:
-    log=f'127.0.0.1 --"GET {random.choice(paths)} HTTP/1.1" 200\n'
-    with open(log_file,"a") as f:
-        f.write(log)
-    time.sleep(1)
