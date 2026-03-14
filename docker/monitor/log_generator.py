493ca99e43acbc493744d1e8b7d5dee62ccb47cd
YADHARDHA LINGALA <125679462+Yadhardha@users.noreply.github.com>
2026-03-14 10:31:00 +0000

    Add files

--- docker/monitor/log_generator.py
+++ docker/monitor/log_generator.py
@@ -1,9 +1,9 @@
-import time,random
-
-f = open("/tmp/devops.log","a")
-
+import time
+import random
+log_file = "/tmp/devops.log"
 paths=["/","/login","/dashboard","/api/data"]
 while True:
-    f.write(f'127.0.0.1 --[] "GET {random.choice(paths)} HTTP/1.1" 200\n')
-    f.flush()
-    time.sleep(2)
+    log=f'127.0.0.1 --"GET {random.choice(paths)} HTTP/1.1" 200\n'
+    with open(log_file,"a") as f:
+        f.write(log)
+    time.sleep(1)
